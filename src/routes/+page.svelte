947f69a54ff572c8f94eee579804a6b035495f94
Richard Gao <rgao473@gmail.com>
2025-07-23 21:49:17 -0700

    added landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+
+</script>
+
+
+<h1 class="flex justify-center text-9xl">Not MySchedule</h1>
+<h3>Yea, not this</h3>
+<img src="/myschedule-vector.svg" alt="A vectorized screenshot of the SFU MySchedule website."/>
